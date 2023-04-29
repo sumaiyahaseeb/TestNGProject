7714cac1ee66910fbaa21cfa777316626a12d3af
sumaiya haseeb <sumaiyaayaz71@gmail.com>
2023-04-29 11:52:02 +0530

    adding the tests

---
+++ src/test/java/testcases/BaseClass.java
@@ -0,0 +1,26 @@
+package testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+	//parent class
+	@BeforeTest(alwaysRun = true)
+	public void method3() {
+		System.out.println("inside before Test");
+	}
+	@BeforeMethod(alwaysRun = true)
+	public void method1() {
+		System.out.println("inside before method");
+	}
+	@AfterMethod(alwaysRun = true)
+	public void method2() {
+		System.out.println("inside after method ");
+	}
+	@AfterTest(alwaysRun = true)
+	public void method4() {
+		System.out.println("inside after Test");
+	}
+}
---
+++ src/test/java/testcases/CreditCard.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class CreditCard extends BaseClass {
+	@Test(groups= {"sanity"})
+	public void test4() {
+		System.out.println("inside creditcard test 4");
+	}
+}
